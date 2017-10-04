0edf0280c0c38d76a82565532a85d0c6c389c2e5
Zack Tylor <taylor.zackory@gmail.com>
2017-10-03 22:52:51 -0500

    create stack class

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ES6-Data-Structures
 ES6 Data structure implementations.
+
+## Table of contents
+[Stack](./stack/stack.js)
---
+++ stack/stack.js
@@ -0,0 +1,10 @@
+class Stack {
+  /*
+  Last in first out
+  */
+  constructor() {
+    // init new stack with a count of 0 and an empty container object.
+    this.count = 0;
+    this.container = {};
+  }
+}
