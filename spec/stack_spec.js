3d0a954ba581acd35b8df7c2e9d257a989b507c0
Zack Tylor <taylor.zackory@gmail.com>
2017-10-08 00:22:34 -0500

    Write tests for the constor and push Stack methods.

--- spec/stack_spec.js
+++ spec/stack_spec.js
@@ -2,15 +2,34 @@ const { Stack } = require('../stack/stack.js');
 describe('Stack', function () {
   let stack;
 
+  beforeEach(function () {
+    stack = new Stack();
+  });
+
   describe('#constructor', function () {
-    it('creates a new stack with a size of 0', function () {
-      stack = new Stack();
+    it('creates a new stack with a size of 0.', function () {
       expect(stack.count).toEqual(0);
     });
 
-    it('creates a new stack with an empty storage', function () {
+    it('creates a new stack with an empty storage.', function () {
+      expect(stack.storage).toEqual({});
+    });
+  });
+
+  describe('#push', function () {
+
+    beforeEach(function () {
       stack = new Stack();
-      expect(stack.container).toEqual({});
+      let newItem = ['New', 'Stuff'];
+      stack.push(newItem);
+    });
+
+    it('adds a new item to the end of the stack.', function () {
+      expect(stack.storage).toEqual({ 0: ['New', 'Stuff'] });
+    });
+
+    it('changes the count of the stack when new item pushed.', function () {
+      expect(stack.count).toEqual(1);
     });
   });
 
