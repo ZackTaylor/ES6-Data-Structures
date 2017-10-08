2994a4e2fbce99f995df61fd3c3dddca018e4744
Zack Tylor <taylor.zackory@gmail.com>
2017-10-08 13:38:17 -0500

    add matchesr and spacing to stack file

--- stack/stack.js
+++ stack/stack.js
@@ -1,7 +1,8 @@
-module.exports.Stack = class Stack {
+module.exports = class Stack {
   /*
   Last in first out
   */
+
   constructor() {
     // init new stack with a count of 0 and an empty container object.
     this.count = 0;
