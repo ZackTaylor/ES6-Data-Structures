701096767b46993652fc14a375c60eecf60a1caa
Zack Tylor <taylor.zackory@gmail.com>
2017-10-09 22:04:47 -0500

    write enqueue method for queue

--- spec/queue_spec.js
+++ spec/queue_spec.js
@@ -13,12 +13,27 @@ describe('Stack', function () {
             expect(queue.count).toEqual(0);
         });
 
-        it('creates a new queue with a lowestIndex of 0.', function () {
-            expect(queue.count).toEqual(0);
+        it('creates a new queue with a tail of 0.', function () {
+            expect(queue.tail).toEqual(0);
         });
 
         it('creates a new queue with an empty storage.', function () {
             expect(queue.storage).toEqual({});
+        });
+    });
+
+    describe('#enqueue', function () {
+
+        beforeEach(function () {
+            queue.enqueue("Test");
+            queue.enqueue("Testing");
+        });
+        it('adds an item to the end of the queue', function () {
+            expect(queue.storage).toEqual({ 0: "Test", 1: "Testing" });
+        });
+
+        it('increments the count', function () {
+            expect(queue.count).toEqual(2);
         });
     });
 
