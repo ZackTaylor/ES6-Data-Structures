0e8276ccd56b197b8d8365add89536aefcd0cf80
Zack Tylor <taylor.zackory@gmail.com>
2017-10-09 22:04:10 -0500

    add tests for queue enqueue method

--- queue/queue.js
+++ queue/queue.js
@@ -2,6 +2,12 @@ module.exports = class Queue {
     constructor() {
         this.storage = {};
         this.count = 0;
-        this.lowestIndex = 0;
+        this.tail = 0;
     }
+
+    enqueue(value) {
+        this.storage[this.count] = value;
+        this.count++;
+    }
+
 };
